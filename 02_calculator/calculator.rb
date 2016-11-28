2e0be7d2c5a5b823c6bc5d5dfef9e4ebd629a9ad
Kieran Davis <davisk4rpi@gmail.com>
2016-11-27 20:49:43 -0800

    solve problem 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,39 @@
 #write your code here
+def add x, y
+	z = x + y
+end
+
+def subtract x, y
+	z = x - y
+end
+
+def sum array
+	l = array.length
+	s=0
+	l.times do |i|
+		s = s + array[i]
+	end
+	return s
+end
+
+def multiply array
+	l = array.length
+	s=1
+	l.times do |i|
+		s = s * array[i]
+	end
+	return s
+end
+
+def power x, y
+	z = x ** y
+end
+
+def factorial x
+	x = x + 1
+	s=0
+	x.times do |i|
+		s = s + i
+	end
+	return s
+end
