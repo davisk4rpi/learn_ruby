fe73ba77a110bf27713ea8844eed8ec58fd37261
Kieran Davis <davisk4rpi@gmail.com>
2016-11-28 19:27:24 -0800

    solve problem 03 weakly

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,43 @@
 #write your code here
+def echo greeting
+	greeting
+end
+
+def shout greeting
+	greeting.upcase
+end
+
+def repeat greeting, x = 2
+	array = []
+	x.times do
+		array.push(greeting)
+	end
+	array.join(" ")
+end
+
+def start_of_word word, x
+	word[0..(x-1)]
+end
+
+def first_word words
+	word = words.split
+	word[0]
+end
+
+def titleize word
+	words = word.split
+	words.each do |each|
+		each.capitalize!
+	end
+	words.each do |each|
+		if each == 'And'
+			each.downcase!
+		elsif each == 'The'
+			each.downcase!
+		elsif each == 'Over'
+			each.downcase!
+		end
+	end
+	words[0].capitalize!
+	words.join(' ')
+end
