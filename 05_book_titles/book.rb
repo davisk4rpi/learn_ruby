8e5d65eecc4ecf34c7f90f8e2cba2ac8fba640e5
Kieran Davis <davisk4rpi@gmail.com>
2016-11-28 20:41:40 -0800

    solve problem 05 books

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
 # write your code here
+	attr_reader :title
+	def title=(new_title)
+		words = new_title.split(" ")
+		new_title = []
+		words.each do |word|
+			lilword = ['a', 'the', 'an', 'and', 'or', 'in', 'of']
+			if lilword.include? word
+				cap_word = word
+			else
+				cap_word = word.capitalize
+			end
+			new_title.push(cap_word)
+		end
+
+
+		new_title[0].capitalize!
+		@title = new_title.join(" ")
+	end
 end
